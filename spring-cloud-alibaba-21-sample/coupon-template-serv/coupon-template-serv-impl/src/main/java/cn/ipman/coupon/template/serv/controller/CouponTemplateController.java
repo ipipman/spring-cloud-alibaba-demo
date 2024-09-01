949639e2da51210e1569f47659b9680d955a2ef2
ipipman <ipipman@163.com>
2024-09-01 22:57:55 +0800

    polish api codes

--- spring-cloud-alibaba-21-sample/coupon-template-serv/coupon-template-serv-impl/src/main/java/cn/ipman/coupon/template/serv/controller/CouponTemplateController.java
+++ spring-cloud-alibaba-21-sample/coupon-template-serv/coupon-template-serv-impl/src/main/java/cn/ipman/coupon/template/serv/controller/CouponTemplateController.java
@@ -14,7 +14,8 @@ import java.util.Collection;
 import java.util.Map;
 
 /**
- * 优惠券模版
+ * 优惠券模版控制器
+ * 提供了对优惠券模版的各种操作，如创建、克隆、读取、批量获取、搜索和删除优惠券模版。
  *
  * @Author IpMan
  * @Date 2024/9/1 22:31
@@ -27,43 +28,73 @@ public class CouponTemplateController {
     @Autowired
     private CouponTemplateService couponTemplateService;
 
-    // 创建优惠券
+    /**
+     * 创建优惠券模版
+     *
+     * @param request 包含优惠券模版信息的请求体
+     * @return 创建完成后的优惠券模版信息
+     */
     @PostMapping("/addTemplate")
     public CouponTemplateInfo addTemplate(@Valid @RequestBody CouponTemplateInfo request) {
         log.info("Create coupon template: data={}", request);
         return couponTemplateService.createTemplate(request);
     }
 
+    /**
+     * 克隆优惠券模版
+     *
+     * @param templateId 需要克隆的优惠券模版ID
+     * @return 克隆后的优惠券模版信息
+     */
     @PostMapping("/cloneTemplate")
     public CouponTemplateInfo cloneTemplate(@RequestParam("id") Long templateId) {
         log.info("Clone coupon template: data={}", templateId);
         return couponTemplateService.cloneTemplate(templateId);
     }
 
-    // 读取优惠券
+    /**
+     * 读取优惠券模版信息
+     *
+     * @param id 优惠券模版ID
+     * @return 优惠券模版详细信息
+     */
     @GetMapping("/getTemplate")
-    public CouponTemplateInfo getTemplate(@RequestParam("id") Long id){
+    public CouponTemplateInfo getTemplate(@RequestParam("id") Long id) {
         log.info("Load template, id={}", id);
         return couponTemplateService.loadTemplateInfo(id);
     }
 
-    // 批量获取
+    /**
+     * 批量获取优惠券模版信息
+     *
+     * @param ids 优惠券模版ID集合
+     * @return 包含指定ID优惠券模版信息的Map，键为模版ID，值为模版信息
+     */
     @GetMapping("/getBatch")
     public Map<Long, CouponTemplateInfo> getTemplateInBatch(@RequestParam("ids") Collection<Long> ids) {
         log.info("getTemplateInBatch: {}", JSON.toJSONString(ids));
         return couponTemplateService.getTemplateInfoMap(ids);
     }
 
-    // 搜索模板
+    /**
+     * 搜索优惠券模版
+     *
+     * @param request 包含搜索条件的请求体
+     * @return 分页的优惠券模版搜索结果
+     */
     @PostMapping("/search")
     public PagedCouponTemplateInfo search(@Valid @RequestBody TemplateSearchParams request) {
         log.info("search templates, payload={}", request);
         return couponTemplateService.search(request);
     }
 
-    // 优惠券无效化
+    /**
+     * 删除优惠券模版
+     *
+     * @param id 需要删除的优惠券模版ID
+     */
     @DeleteMapping("/deleteTemplate")
-    public void deleteTemplate(@RequestParam("id") Long id){
+    public void deleteTemplate(@RequestParam("id") Long id) {
         log.info("Load template, id={}", id);
         couponTemplateService.deleteTemplate(id);
     }
