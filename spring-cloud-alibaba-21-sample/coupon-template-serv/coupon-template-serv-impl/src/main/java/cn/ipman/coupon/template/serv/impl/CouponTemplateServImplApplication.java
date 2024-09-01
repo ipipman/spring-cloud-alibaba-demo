72f7bd3c43352aab339003f8e4fa14443d7798b9
ipipman <ipipman@163.com>
2024-09-01 21:45:18 +0800

    add coupon server impl layer

---
+++ spring-cloud-alibaba-21-sample/coupon-template-serv/coupon-template-serv-impl/src/main/java/cn/ipman/coupon/template/serv/impl/CouponTemplateServImplApplication.java
@@ -0,0 +1,13 @@
+package cn.ipman.coupon.template.serv.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponTemplateServImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponTemplateServImplApplication.class, args);
+    }
+
+}
