37180b47b2771948a0509b8d760b4e9f73897e8f
ipipman <ipipman@163.com>
2024-09-08 13:30:51 +0800

    add calculation implement module

---
+++ spring-cloud-alibaba-21-sample/coupon-calculation-serv/coupon-calculation-serv-impl/src/main/java/cn/ipman/coupon/calculation/serv/Application.java
@@ -0,0 +1,13 @@
+package cn.ipman.coupon.calculation.serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
